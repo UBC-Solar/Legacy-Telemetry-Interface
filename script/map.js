d9e555f2013a35568ba6b48a01acad6024dbf886
Harry <honeytung@gmail.com>
2019-11-02 12:50:55 -0700

    Added google maps beta api

---
+++ script/map.js
@@ -0,0 +1,8 @@
+// Simple Google Map API
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(51.508742, -0.120850),
+        zoom: 5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
