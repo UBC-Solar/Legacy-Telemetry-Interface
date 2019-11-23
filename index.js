b165efbc562b7fdcbdfcf2d588240c66af44e198
Harry <honeytung@gmail.com>
2019-11-23 14:22:25 -0800

    Code Cleanup

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
 server.listen(
-    port, 
-    function() 
-    {
+    port,
+    function () {
         console.log('Server listening at port %d', port);
     }
 );
@@ -18,8 +17,7 @@ app.use(express.static(path.join(__dirname, '/client')));
 
 io.on(
     'connection',
-    function(socket)
-    {
+    function (socket) {
         console.log("Initial Message Sent");
         io.emit('introduction');
     }
@@ -32,11 +30,11 @@ var parseData = require("./CANparse.js");
 //      parses it to its components and sends those as a JSON file.
 var rawID = 0x622
 setInterval(
-    function(){
+    function () {
 
         var rawData = generateRawData(rawID);
         var data = parseData.parseRaw(rawData);
-        
+
         var id = data["ID"];
         console.log(id);
         if (id === 0x622) {
@@ -77,18 +75,17 @@ setInterval(
 )
 
 
-function generateRawData(id)
-{
+function generateRawData(id) {
     var aveTemp = Math.floor(Math.random() * 100);
     var maxTemp = Math.floor(Math.random() * 100);
     var minTemp = Math.floor(Math.random() * 100);
 
-    var rawData = 
+    var rawData =
     {
         "id": id,
-        "data": [ (aveTemp & 0xFF), 0x00, (minTemp & 0xFF), 0x03, (maxTemp & 0xFF), 0x05, 0x06, 0x07],
+        "data": [(aveTemp & 0xFF), 0x00, (minTemp & 0xFF), 0x03, (maxTemp & 0xFF), 0x05, 0x06, 0x07],
         "len": 8,
-        "timestamp": new Date().getTime() 
+        "timestamp": new Date().getTime()
     }
 
     console.log("raw data generated");
