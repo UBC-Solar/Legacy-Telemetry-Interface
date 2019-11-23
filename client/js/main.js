0f8a0fc2affa7d04a62369a7ac801a77b76a1db2
Harry <honeytung@gmail.com>
2019-11-23 12:14:17 -0800

    Code Cleanup

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,5 @@
 
-window.onload = function()
-{
+window.onload = function () {
 
     var serverConnStatus = document.getElementById('ServerConnectStatus');
     var aveBattTemp = document.getElementById('ave-batt-temp');
@@ -8,29 +7,27 @@ window.onload = function()
     var maxBattTemp = document.getElementById('max-batt-temp');
     var maxBattTempTS = document.getElementById('max-batt-temp-ts');
     var minBattTemp = document.getElementById('min-batt-temp');
-    var minBattTempTS = document.getElementById('min-batt-temp-ts');    
+    var minBattTempTS = document.getElementById('min-batt-temp-ts');
 
     var socket = io();
 
     socket.on(
         'introduction',
-        function(data)
-        {
+        function (data) {
             serverConnStatus.innerHTML = "Connected";
             serverConnStatus.style.color = "green";
         }
     )
-    
+
     socket.on(
-        'battery-temp-msg', 
-        function(data)
-        {
+        'battery-temp-msg',
+        function (data) {
             aveBattTemp.innerHTML = data['data']['ave-batt-temp'];;
             aveBattTempTS.innerHTML = data['timestamp'];
 
             maxBattTemp.innerHTML = data['data']['max-batt-temp'];
             maxBattTempTS.innerHTML = data['timestamp'];
-            
+
             minBattTemp.innerHTML = data['data']['min-batt-temp'];
             minBattTempTS.innerHTML = data['timestamp'];
         }
