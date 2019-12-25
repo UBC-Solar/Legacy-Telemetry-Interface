aa575f87c169b625aa782ad4a2ff9b7d67e55ad0
DavidWidjaja-137 <david.widjaja@alumni.ubc.ca>
2019-12-24 23:24:14 -0500

    Revised the user interface for optimal use of space

--- CAN Specifications and Parsing/CANparse.js
+++ CAN Specifications and Parsing/CANparse.js
@@ -4,14 +4,18 @@ function parseRaw(receive) {
     var milli = receive["timestamp"];
 
     if (id == 0x622) {
+
+        //Msg 0x622, Byte 0: State of System
         var relayFault = (message >> 59 & 1);
         var K3 = (message >> 60 & 1);
         var K2 = (message >> 61 & 1);
         var K1 = (message >> 62 & 1);
         var faultState = (message >> 63 & 1);
 
+        //Msg 0x522, Byte 1, Byte 2: Timer
         var timer = message[1].concat(message[2]);
 
+        //Msg 0x622, Byte 4: Fault Codes
         var fanOn = (message >> 32 & 1);
         var LLIM = (message >> 33 & 1);
         var HLIM = (message >> 34 & 1);
@@ -21,6 +25,7 @@ function parseRaw(receive) {
         var powerLoad = (message >> 38 & 1);
         var powerSource = (message >> 39 & 1);
 
+        //Msg 0x622, Byte 5: Level Faults
         var overVoltage = (message[5] >> 7 & 1);
         var underVoltage = (message[5] >> 6 & 1);
         var overTemperature = (message[5] >> 5 & 1);
@@ -30,6 +35,7 @@ function parseRaw(receive) {
         var interlockTrip = (message[5] >> 1 & 1);
         var drivingOff = (message[5] >> 0 & 1);
 
+        //Msg 0x622, Byte 6: Warnings
         var isoFault = (message[6] >> 7 & 1);
         var lowSOH = (message[6] >> 6 & 1);
         var hotTemp = (message[6] >> 5 & 1);
@@ -39,11 +45,10 @@ function parseRaw(receive) {
         var highVolt = (message[6] >> 1 & 1);
         var lowVolt = (message[6] >> 0 & 1);
 
-
         var send =
         {
             "ID": id,
-			"TimeStamp": milli,
+			"timeStamp": milli,
 			"relayFault": relayFault,
             "K3": K3,
             "K2": K2,
@@ -75,18 +80,19 @@ function parseRaw(receive) {
             "highVolt": highVolt,
             "lowVolt": lowVolt
         }
+
         return send;
 
     } else if (id == 0x623) {
-        var packVoltage = receive["data"][0].concat(receive["data"][1]); // how to make sure js reads as unsigned?
+
+        var packVoltage = (receive["data"][0] << 8) | receive["data"][1];
         var minVoltage = receive["data"][2];
         var maxVoltage = receive["data"][4];
-
 
         var send =
         {
             "ID": 0x623,
-            "TimeStamp": milli,
+            "timeStamp": milli,
             "packVoltage": packVoltage,
             "minVoltage": minVoltage,
             "maxVoltage": maxVoltage
@@ -95,12 +101,13 @@ function parseRaw(receive) {
         return send;
 
     } else if (id == 0x624) {
-        var current = receive["data"][0].concat(receive["data"][1]);
+
+        var current = (receive["data"][0] << 8) | receive["data"][1];
 
         var send =
         {
             "ID": 0x624,
-            "TimeStamp": milli,
+            "timeStamp": milli,
             "current": current
         }
 
@@ -108,28 +115,28 @@ function parseRaw(receive) {
 
     } else if (id == 0x626) {
 
-        var SOC = recieve["data"][0];
+        var SOC = receive["data"][0];
 
         var send =
         {
             "ID": 0x626,
-            "TimeStamp": milli,
-            "SOC": SOC
+            "timeStamp": milli,
+            "stateOfCharge": SOC
         }
 
         return send;
 
     } else if (id == 0x627) {
 
-        var temperature = recieve["data"][0];
-        var minTemp = recieve["data"][2];
+        var temperature = receive["data"][0];
+        var minTemp = receive["data"][2];
         var maxTemp = receive["data"][4];
 
         var send =
         {
             "ID": 0x627,
-            "TimeStamp": milli,
-            "Temperature": temperature,
+            "timeStamp": milli,
+            "temperature": temperature,
             "minTemp": minTemp,
             "maxTemp": maxTemp
 
@@ -150,27 +157,27 @@ function parseRaw(receive) {
         var send =
         {
             "ID": "0x401",
-            "TimeStamp": milli,
+            "timeStamp": milli,
             "voltageLockOut": voltageLockOut,
             "configError": configError,
             "watchdogReset": watchdogReset,
             "badMotor": badMotor,
             "DCOverVoltage": DCOverVoltage,
-            "softwareOverCurrent": softwareOvercurrent,
-            "hardwareOverCurrent": hardwareOvercurrent
+            "softwareOverCurrent": softwareOverCurrent,
+            "hardwareOverCurrent": hardwareOverCurrent
         }
 
         return send;
 
     } else if (id == 0x402) {
-
-        var busCurrent = receive["data"][0].concat(receive["data"][1]).concat(receive["data"][2]).concat(receive["data"][3]);//unsure about this
-        var busVoltage = receive["data"][4].concat(receive["data"][5]).concat(receive["data"][6]).concat(receive["data"][7]);
+        
+        var busCurrent = ieee32ToFloat((receive["data"][0] << 24) | (receive["data"][1] << 16) | (receive["data"][2] << 8) | (receive["data"][3]));
+        var busVoltage = ieee32ToFloat((receive["data"][4] << 24) | (receive["data"][5] << 16) | (receive["data"][6] << 8) | (receive["data"][7]));
 
         var send =
         {
             "ID": "0x402",
-            "TimeStamp": milli,
+            "timeStamp": milli,
             "busCurrent": busCurrent,
             "busVoltage": busVoltage
         }
@@ -179,12 +186,12 @@ function parseRaw(receive) {
 
     } else if (id == 0x403) {
 
-        var velocity = receive["data"][0].concat(receive["data"][1]).concat(receive["data"][2]).concat(receive["data"][3]);
+        var velocity = ieee32ToFloat((receive["data"][0] << 24) | (receive["data"][1] << 16) | (receive["data"][2] << 8) | (receive["data"][3]));
 
         var send =
         {
             "ID": "0x403",
-            "TimeStamp": milli,
+            "timeStamp": milli,
             "velocity": velocity
         }
 
@@ -195,9 +202,36 @@ function parseRaw(receive) {
         var send = {
 
             "ID": recieve["id"],
-            "Data Type": "unknown ID"
+            "timestamp": milli,
+            "msg": "unknown"
         }
 
         return send;
     }
 }
+
+function ieee32ToFloat(intval) {
+    var fval = 0.0;
+    var x;//exponent
+    var m;//mantissa
+    var s;//sign
+    s = (intval & 0x80000000)?-1:1;
+    x = ((intval >> 23) & 0xFF);
+    m = (intval & 0x7FFFFF);
+    switch(x) {
+        case 0:
+            //zero, do nothing, ignore negative zero and subnormals
+            break;
+        case 0xFF:
+            if (m) fval = NaN;
+            else if (s > 0) fval = Number.POSITIVE_INFINITY;
+            else fval = Number.NEGATIVE_INFINITY;
+            break;
+        default:
+            x -= 127;
+            m += 0x800000;
+            fval = s * (m / 8388608.0) * Math.pow(2, x);
+            break;
+    }
+    return fval;
+} 
